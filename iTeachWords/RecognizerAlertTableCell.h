f8cb2a254ab5dec08b5a539a29699b1f19c55d31
Vitaly Todorovych (iMac Lion) <vitaly.todorovych@yandex.ru>
2012-06-19 18:20:45 +0300

    icons changes

---
+++ iTeachWords/RecognizerAlertTableCell.h
@@ -0,0 +1,13 @@
+//
+//  RecognizerAlertTableCell.h
+//  iTeachWords
+//
+//  Created by Vitaly Todorovych on 19.06.12.
+//  Copyright (c) 2012 OSDN. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RecognizerAlertTableCell : UITableViewCell
+
+@end
---
+++ iTeachWords/RecognizerAlertTableCell.m
@@ -0,0 +1,29 @@
+//
+//  RecognizerAlertTableCell.m
+//  iTeachWords
+//
+//  Created by Vitaly Todorovych on 19.06.12.
+//  Copyright (c) 2012 OSDN. All rights reserved.
+//
+
+#import "RecognizerAlertTableCell.h"
+
+@implementation RecognizerAlertTableCell
+
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+{
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
